ba645c68ca7f7ba5d8b4c2894deaf2d31a62753e
Victor-Merino-4 <victor.merino.7e6@itb.cat>
2025-03-26 17:43:03 +0100

    login and register reloaded

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,25 @@
+import { Link } from "lucide-react";
 import React from "react";
 
 
 export default function Home() {
   return (
     <div className="bg-blue-50 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="bg-center h">
+        <h1 className="text-black text-6xl">Registrarse</h1>
+      </div>
+      <div id="register" className="container w-60 border-2 border-gray-400">
+        <form action="#" method="POST">
+          <label htmlFor="Nombre">Nombre</label>
+          <input type="text" name="name" placeholder="ex:Pepito Ojeda" required />
+          <label htmlFor="Correo">Correo</label>
+          <input type="email" name="email" placeholder="ex: pepito.ojeda@gmail.com" required />
+          <label htmlFor="Contraseña">Contraseña</label>
+          <input type="password" name="password" placeholder="***************" required />
+          <button>Registrarse</button>
+        </form>
+        <Link href="/login">Ya estas registrado? Inicia sesión</Link>        
+      </div>
     </div>
   );
 }
